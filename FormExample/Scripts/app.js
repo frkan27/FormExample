9d59a5151f2f4cbaaafc94b577530fff224f3c1f
Furkan Kılıç <furkankilicytu@gmail.com>
2019-04-16 23:13:33 +0300

    Formgeliyor

---
+++ FormExample/Scripts/app.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="angular.js" />
+
+var app = angular.module("app", []);
+
+app.directive("userCard", function () {
+    return {
+        restrict: 'EAC',
+        templateUrl: '/Index.cshtml'
+    };
+});
